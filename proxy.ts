8da99f2d2fe8d1f4834b244140241d67fb1893e1
Luis Jaco <lmario329@gmail.com>
2026-04-03 09:22:36 -0400

    Configure proxy to enforce user session.

--- proxy.ts
+++ proxy.ts
@@ -6,13 +6,26 @@ import { type NextRequest } from "next/server"
 import { updateSession } from "@/lib/supabase/proxy"
 
 export async function proxy(request: NextRequest) {
+  // skip root check.
+  const { pathname } = request.nextUrl
+  if (pathname === '/') return;
+
   // refer to @/lib/supabase/proxy if you need to edit the redirect.
   return await updateSession(request)
 }
 
 export const config = {
   matcher: [
-    "/dashboard",
-    "/onboarding",
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     * - login
+     * - signup
+     * - root (handled in proxy function itself.)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|login|signup).*)',
   ],
 }
